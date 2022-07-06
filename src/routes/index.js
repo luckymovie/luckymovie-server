02b4370678546c953ac9e3e1dc3ad140d5767f0f
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-07-07 00:40:46 +0700

    Add auht router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
+const express = require("express");
 const Router = require("express").Router();
+const authRouter = require("./auth");
 
 Router.get("/", (_req, res) => {
   res.json({
     message: "This is Lucky Movie API",
   });
 });
-// Router.use("/auth");
+Router.use("/auth", authRouter);
 // Router.use("/user");
 // Router.use("/movies");
 // Router.use("/transaction");
