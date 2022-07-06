61f1e123fac0681dc36e8466663408edd6839bb2
kondelow <achmadrasidi@gmail.com>
2022-07-06 16:23:29 +0700

    add mvc server,config db

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const { dbConn } = require("./src/config/db");
+const mainRouter = require("./src/routes/index");
 
 const PORT = process.env.PORT;
+dbConn();
 
-app.get("/", (_req, res) => {
-  res.json({
-    message: "This is lucky movie API",
-  });
-});
+app.use(mainRouter);
 
 app.listen(PORT, console.log(`Server is Running at port ${PORT}`));
