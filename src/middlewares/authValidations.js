081e4a0a2f88da05b7c4ce8aa617fbcf2d2b37da
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-07-07 19:18:41 +0700

    Add forgot and reset password

--- src/middlewares/authValidations.js
+++ src/middlewares/authValidations.js
@@ -60,6 +60,59 @@ const checkSigInForm = [
     },
 ];
 
+const forgot = [
+    body("email")
+        .isEmail()
+        .withMessage("Please enter a valid email address")
+        .normalizeEmail()
+];
+
+const checkForgotForm = [
+    forgot,
+    (req, res, next) => {
+      const error = validationResult(req);
+      if (!error.isEmpty()) {
+        return errorResponse(res, 400, {msg: error.array()})
+      }
+      next();
+    },
+];
+
+const checkEmail = (req, res, next) => {
+    getEmail(req.body.email)
+    .then((result) => {
+        if(result.rowCount === 0){
+            return errorResponse(res, 400, {msg: "Email is not registered"});
+        }
+        req.id = result.rows[0].id
+        next();
+    })
+    .catch((error) => {
+        const {status, err} = error;
+        errorResponse(res, status, err);
+    });
+};
+
+
+const reset = [
+    body("newPassword")
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 characters")
+        .matches(/\d/)
+        .withMessage("Password must contain a number")
+];
+
+const checkResetForm = [
+    reset,
+    (req, res, next) => {
+      const error = validationResult(req);
+      if (!error.isEmpty()) {
+        return errorResponse(res, 400, {msg: error.array()})
+      }
+      next();
+    },
+];
+ 
 module.exports = {
-    checkRegistedEmail, checkRegisterForm, checkSigInForm
+    checkRegistedEmail, checkRegisterForm, checkSigInForm, checkForgotForm, checkEmail, checkResetForm
 };
