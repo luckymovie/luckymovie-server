d9ceec2e7638d4cbcc2eb75a9535cc474471696f
kondelow <achmadrasidi@gmail.com>
2022-07-11 10:00:46 +0700

    edit query get movie

--- src/models/movies.js
+++ src/models/movies.js
@@ -19,7 +19,7 @@ const postMovie = async (body, image) => {
 const getMovieNow = async () => {
   try {
     const sqlQuery =
-      "SELECT distinct on(m.id)c.movies_id ,title,genre,duration,casts,synopsis,director,date(release_date) as release_date,image,c.price as price, c.name as cinema_name,t.time as time , c.date as date,l.city as city,l.address as address FROM movies m join cinemas c on c.movies_id = m.id join times t on t.id = c.times_id join location l on l.id = c.location_id WHERE date_part('day',release_date) between date_part('day',release_date) and date_part('day',release_date)+30 GROUP by date(release_date),m.id,m.title,m.genre,m.duration,m.casts,m.synopsis,m.director,m.image,c.name,c.price,c.movies_id ,t.time,c.date,l.city,l.address ";
+      "SELECT distinct on(m.id)c.movies_id ,title,genre,duration,casts,synopsis,director,date(release_date) as release_date,image,c.price as price, c.name as cinema_name,t.time as time , c.date as date,l.city as city,l.address as address FROM movies m join cinemas c on c.movies_id = m.id join times t on t.id = c.times_id join location l on l.id = c.location_id WHERE date_part('month',release_date) between date_part('month',now())-1 and date_part('month',now())+1 GROUP by date(release_date),m.id,m.title,m.genre,m.duration,m.casts,m.synopsis,m.director,m.image,c.name,c.price,c.movies_id ,t.time,c.date,l.city,l.address";
     const result = await db.query(sqlQuery);
     if (!result.rowCount) throw new ErrorHandler({ status: 404, message: "Movies not found" });
     return {
@@ -32,7 +32,10 @@ const getMovieNow = async () => {
 };
 
 const getMovieUpcoming = async (query) => {
-  const { month = 8 } = query;
+  const date = new Date();
+  const m = date.getMonth() + 3;
+  const { month = m } = query;
+
   try {
     const sqlQuery =
       "SELECT distinct on(m.id)c.movies_id,title,genre,duration,casts,synopsis,director,date(release_date),image,c.price as price, c.name as cinema_name,t.time as time,c.date as date,l.city as city,l.address as address FROM movies m join cinemas c on c.movies_id = m.id join times t on t.id = c.times_id join location l on l.id = c.location_id  WHERE date_part('month',release_date) = $1 GROUP by date(release_date),m.title,m.genre,m.duration,m.casts,m.synopsis,c.movies_id,m.director,m.image,c.name,c.price,m.id,t.time,c.date,l.city,l.address";
