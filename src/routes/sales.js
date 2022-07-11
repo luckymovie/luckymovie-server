21496d310967a07731d13cd556f786d3044986ca
Lukmandst <lukmandwi11@gmail.com>
2022-07-12 04:49:55 +0700

    add new route for new get sales

--- src/routes/sales.js
+++ src/routes/sales.js
@@ -3,8 +3,9 @@ const Router = express.Router();
 const authController = require("../controllers/sales");
 const tokenValidation = require("../middlewares/tokenValidations");
 
+// Get All movie sales
+Router.get("/all", authController.getAllMoviesSales);
 //Get sales
 Router.get("/", tokenValidation.checkToken, authController.getSales);
-
 
 module.exports = Router;
